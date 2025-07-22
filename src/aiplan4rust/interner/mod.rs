df5fc917f0ff2f29d9cccb2030c5760f52d8cb1f
pellierd <damien.pellier@imag.fr>
2025-07-22 15:12:50 +0200

    Rename string.rs in interner.rs

--- src/aiplan4rust/interner/mod.rs
+++ src/aiplan4rust/interner/mod.rs
@@ -1,7 +1,7 @@
-pub mod string;
+pub mod interner;
 pub mod merged_result;
 pub mod display;
 
-pub use string::StringInterner;
+pub use interner::StringInterner;
 pub use merged_result::InternerMergeResult;
 pub use display::InternerDisplay;
