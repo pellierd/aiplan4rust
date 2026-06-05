cc450eb448cce2257e2500357b540f85cbe78456
pellierd <damien.pellier@imag.fr>
2026-06-06 00:35:18 +0200

    refactor(grounding): clean binding module

--- src/aiplan4rust/grounding/binding/mod.rs
+++ src/aiplan4rust/grounding/binding/mod.rs
@@ -1,6 +1,3 @@
-pub mod apply;
-pub mod bindable;
-pub mod binder;
 pub mod bindings;
 mod error;
 pub mod evaluator;
@@ -8,15 +5,9 @@ mod expr;
 pub mod iter;
 mod scratchpad;
 
-pub use bindable::Bindable;
-pub use binder::ExprBinder;
 pub use bindings::Bindings;
 pub use error::BindingError;
 pub use scratchpad::BindingScratchpad;
 
-pub use apply::apply;
-pub use apply::apply_in_place;
-pub use apply::apply_in_place_with;
-pub use apply::apply_with;
 pub use expr::bind;
 pub use expr::bind_with;
